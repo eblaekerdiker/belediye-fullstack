4df019a56c825d6daa79ae4a8a62e26982cc3324
eblaekerdiker <eblaekerdiker@gmail.com>
2025-04-15 13:27:19 +0300

    Proje tamamlandı

--- pages/api/kullanicilar.js
+++ pages/api/kullanicilar.js
@@ -1,3 +1,4 @@
+import { data } from 'autoprefixer';
 import connectDB from '../../lib/db';
 import User from '../../models/User';
 
@@ -7,6 +8,7 @@ export default async function handler(req, res) {
     if (req.method === 'POST'){
         try{
             const {ad, email} = req.body;
+            console.log("POST isteği alındı:", ad, email);
             const yeniKullanici = new User({ad,email});
             await yeniKullanici.save();
             return res.status(201).json({success: true, data: yeniKullanici}); 
@@ -24,7 +26,16 @@ export default async function handler(req, res) {
         }
     }
 
+    if(req.method === 'DELETE') {
+        try {
+            const {id} = req.body;
+            const sonuc = await User.findByIdAndDelete(id);
+            return res.status(200).json({success:true, data:sonuc});            
+        }catch(error){
+            return res.status(500).json({success:false, error:error.message});
+        }
+    }
+
     res.setHeader('Allow', ['GET', 'POST']);
     res.status(405).end('Method ${res.method} Not Allowed');
-
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,90 @@
-export async function getServerSideProps() {
-  const res = await fetch ("http://localhost:3000/api/kullanicilar");
-  const data = await res.json();
+import { use, useState } from "react";
 
+export async function getServerSideProps() {
+  
+  const res = await fetch("http://localhost:3000/api/kullanicilar");
+  const data = await res.json();
 
   return {
     props: {
-      kullanicilar : data.data,
+      kullanicilar: data.data,
     },
   };
-  
 }
 
-export default function Home({kullanicilar}){
+export default function Home({kullanicilar}) {
+
+  const [ad, setAd] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    const res = await fetch("/api/kullanicilar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ad, email }),
+    })
+      .then((res) => {
+        console.log("🎯 İstek gönderildi! Status:", res.status);
+        return res.json();
+      })
+      .catch((err) => {
+        console.error("❌ FETCH HATASI:", err);
+      });
+    
+  };
+
+  const handleDelete = async (id) => {
+    const confirmed = confirm("Bu kullaniciyi silmek istiyor musun?");
+    if (!confirmed) return;
+
+    const res = await fetch("/api/kullanicilar", {
+      method:"DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({id}),
+    });
+
+    if(res.ok){
+      window.location.reload();
+    }
+  };
+
   return(
     <main className="p-6 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Kullanici Listesi</h1>
-      <ul className="space-y-2">
-        {kullanicilar.map((k,i)=>(
+      <ul className="space-y-2 mb-6">
+        {kullanicilar.map((k,i) => (
           <li key={i} className="border p-2 rounded bg-gray-100">
             <strong>{k.ad}</strong> - {k.email}
+            <button onClick={() => handleDelete(k._id)} className="text-red-600 hover:underline px-4">
+              Sil
+            </button>
           </li>
         ))}
       </ul>
-    </main>
-  )
-}
 
+      <form onSubmit={handleSubmit} className="space-y-3">
+        <input
+        type="text"
+        placeholder="Ad"
+        className="w-full border px-4 py-2 rounded"
+        value={ad}
+        onChange={(e) => setAd(e.target.value)}
+        required
+        />
+         <input
+        type="email"
+        placeholder="Email"
+        className="w-full border px-4 py-2 rounded"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        />
+        <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Ekle</button>
+      </form>
+    </main>
+  );
+}
